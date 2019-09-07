e891c7ddd7b16f07cca755e747480c4cf8f48fdc
Rakshit <rak1729e@gmail.com>
2019-09-07 12:05:31 +0530

    minor refractor

---
+++ src/java/generics/PairGenericWithComparator.java
@@ -0,0 +1,30 @@
+package java.generics;
+
+public class PairGenericWithComparator {
+    class pair<T, W> implements Comparable<pair>{
+        private T fi;
+        private W se;
+
+        public pair() {
+
+        }
+        public pair(T fi , W se) {
+            this.fi = fi;
+            this.se = se;
+        }
+
+        public T getFi() {
+            return fi;
+        }
+
+        public W getSe() {
+            return se;
+        }
+
+        @Override
+        public int compareTo(pair o) {
+            if(fi != o.fi) return Long.valueOf((Long) fi).compareTo(Long.valueOf((Long) o.fi));
+            return Long.valueOf((Long) se).compareTo((Long.valueOf((Long) o.fi)));
+        }
+    }
+}
